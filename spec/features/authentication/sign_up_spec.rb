3d2997ff1801815a9781bcf91926a0085252cca4
EugenePetrik <eugeniy.petrik@gmail.com>
2020-01-24 18:08:07 +0200

    Add tests for Sign Up page

---
+++ spec/features/authentication/sign_up_spec.rb
@@ -0,0 +1,119 @@
+RSpec.describe 'SignUp' do
+  let(:sign_up_page) { SignUpPage.new }
+  let(:home_page) { HomePage.new }
+
+  let(:email) { FFaker::Internet.email }
+  let(:password) { FFaker::Internet.password }
+
+  let(:params_signup_data) do
+    {
+      email: email,
+      password: password,
+      password_confirmation: password
+    }
+  end
+
+  let(:success_message) { 'Welcome! You have signed up successfully.' }
+  let(:error_email_blank_invalid) { "Email can't be blank and Email is invalid" }
+  let(:error_email_invalid) { 'Email is invalid' }
+  let(:error_pass_blank) { "Password can't be blank" }
+  let(:error_pass_less_chars) { '6 characters minimum' }
+  let(:error_pass_short) { 'Password is too short (minimum is 6 characters)' }
+  let(:error_pass_confirm_match) { "Password confirmation doesn't match Password" }
+
+  before { sign_up_page.load }
+
+  context 'when open page', :smoke do
+    it { expect(sign_up_page).to be_displayed }
+    it { expect(sign_up_page).to be_all_there }
+    it { expect(sign_up_page.title).to eq('Bookstore') }
+  end
+
+  context 'with valid data' do
+    it 'user signs up' do
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(home_page).to be_displayed
+      expect(home_page.success_flash.text).to eq(success_message)
+    end
+  end
+
+  context 'with empty data' do
+    it 'raises an error' do
+      params_signup_data[:email] = ''
+      params_signup_data[:password] = ''
+      params_signup_data[:password_confirmation] = ''
+
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(sign_up_page.email_error.text).to eq(error_email_blank_invalid)
+      expect(sign_up_page.pass_error.text).to eq(error_pass_blank)
+      expect(sign_up_page.pass_less_6_chars_error.text).to eq(error_pass_less_chars)
+    end
+  end
+
+  context 'with invalid email format' do
+    it 'raises an error' do
+      params_signup_data[:email] = 'testexample.com'
+
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(sign_up_page.email_error.text).to eq(error_email_invalid)
+    end
+  end
+
+  context 'with empty email' do
+    it 'raises an error' do
+      params_signup_data[:email] = ''
+
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(sign_up_page.email_error.text).to eq(error_email_blank_invalid)
+    end
+  end
+
+  context 'with empty password' do
+    it 'raises an error' do
+      params_signup_data[:password] = ''
+
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(sign_up_page.pass_error.text).to eq(error_pass_blank)
+      expect(sign_up_page.pass_less_6_chars_error.text).to eq(error_pass_less_chars)
+      expect(sign_up_page.pass_confirm_error.text).to eq(error_pass_confirm_match)
+    end
+  end
+
+  context 'with password less than 6 characters' do
+    it 'raises an error' do
+      params_signup_data[:password] = '12345'
+      params_signup_data[:password_confirmation] = '12345'
+
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(sign_up_page.pass_error.text).to eq(error_pass_short)
+      expect(sign_up_page.pass_less_6_chars_error.text).to eq(error_pass_less_chars)
+    end
+  end
+
+  context 'with empty password confimation' do
+    it 'raises an error' do
+      params_signup_data[:password_confirmation] = ''
+
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(sign_up_page.pass_confirm_error.text).to eq(error_pass_confirm_match)
+    end
+  end
+
+  context 'with different passwords' do
+    it 'raises an error' do
+      params_signup_data[:password] = "   #{password}   "
+      params_signup_data[:password_confirmation] = password
+
+      sign_up_page.sign_up_with(params_signup_data)
+
+      expect(sign_up_page.pass_confirm_error.text).to eq(error_pass_confirm_match)
+    end
+  end
+end
---
+++ spec/support/pages/authentication/sign_up_page.rb
@@ -0,0 +1,26 @@
+class SignUpPage < BasePage
+  set_url '/users/sign_up'
+
+  element :fb_icon, 'a.general-login-icon'
+  element :sign_up_title, 'h3.general-login-title', text: 'Sign up'
+  
+  element :email, '#user_email'
+  element :password, '#user_password'
+  element :password_confirmation, '#user_password_confirmation'
+  element :sign_up_button, '[name="commit"]'
+
+  expected_elements :fb_icon, :sign_up_title, :email, :password, :password_confirmation, :sign_up_button
+
+  # Errors messages
+  element :email_error, '[for="user_email"]+.invalid-feedback'
+  element :pass_error, '[for="user_password"]+.invalid-feedback'
+  element :pass_less_6_chars_error, :xpath, '//*[@for="user_password"]/../..//small'
+  element :pass_confirm_error, '[for="user_password_confirmation"]+.invalid-feedback'
+
+  def sign_up_with(options = {})
+    email.set(options[:email])
+    password.set(options[:password])
+    password_confirmation.set(options[:password_confirmation])
+    sign_up_button.click
+  end
+end
