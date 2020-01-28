b9d7c45dd52f074056fe3d0b9bbd0dd3910798d1
EugenePetrik <eugeniy.petrik@gmail.com>
2020-01-28 18:05:26 +0200

    Add tests for log out

--- spec/support/pages/home_page.rb
+++ spec/support/pages/home_page.rb
@@ -5,6 +5,8 @@ class HomePage < BasePage
   element :login_link, 'a[href="/users/sign_in"]'
 
   element :user_email, '.user_email > a'
+  element :setting, 'a[href="/settings"]'
+  element :log_out, 'a[href="/users/sign_out"]'
 
   element :filters_title, '#filters h1'
   element :catalog_title, 'h1.general-title-margin'
@@ -44,5 +46,10 @@ class HomePage < BasePage
 
   def books_titles
     book_title.map(&:text)
+  end
+
+  def user_log_out
+    user_email.hover
+    log_out.click
   end
 end
